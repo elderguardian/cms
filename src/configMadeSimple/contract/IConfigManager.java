ecf7301f6fa8a984f5abf55c64adbb1d0cd36bf0
elderguardian <katzenmaedchen@tutanota.com>
2024-02-01 20:47:37 +0100

    create iconfigmanager contract

--- src/configMadeSimple/contract/IConfigManager.java
+++ src/configMadeSimple/contract/IConfigManager.java
@@ -1,2 +1,6 @@
-package configMadeSimple.contract;public interface IConfigManager {
+package configMadeSimple.contract;
+
+public interface IConfigManager<T extends Enum<T> & IConfigKeys>  {
+    IConfig<T> create(String path) throws Exception;
+    void save(IConfig<T> config, String path) throws Exception;
 }
