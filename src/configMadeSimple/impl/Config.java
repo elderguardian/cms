a67bcef7f36635dba1950b861b1ec366eba5db75
elderguardian <katzenmaedchen@tutanota.com>
2024-03-06 15:56:46 +0100

    implement config

--- src/configMadeSimple/impl/Config.java
+++ src/configMadeSimple/impl/Config.java
@@ -1,4 +1,35 @@
 package configMadeSimple.impl;
 
-public class Config {
+import configMadeSimple.contract.IConfig;
+import configMadeSimple.contract.IConfigKeys;
+
+import java.io.Serializable;
+import java.util.Properties;
+
+public class Config<T extends Enum<T> & IConfigKeys> implements IConfig<T> {
+    private final Properties properties;
+
+    public Config() {
+        this.properties = new Properties();
+    }
+
+    public Config(Properties properties) {
+        this.properties = properties;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    @Override
+    public Serializable getObject(T key) {
+        Serializable value = properties.getProperty(key.toString());
+        return (value != null) ? value : key.getDefaultValue();
+    }
+
+    @Override
+    public IConfig<T> setObject(T key, Serializable object) {
+        properties.setProperty(key.toString(), object.toString());
+        return this;
+    }
 }
